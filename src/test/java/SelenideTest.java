d7280c72ba97e1af474b438a43dc04a5d05828ea
Vasiliy Volishevskiy <Volishevski.V@gmail.com>
2022-12-08 13:11:10 +0300

    added open

--- src/test/java/SelenideTest.java
+++ src/test/java/SelenideTest.java
@@ -1,2 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+
 public class SelenideTest {
+    @Test
+    public void testIssue() {
+        open("https://github.com/");
+    }
+
 }
